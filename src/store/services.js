d4c290b2595822e289bc11eaa1d59c5b6fd6d464
Crowdbotics <team@crowdbotics.com>
2020-09-07 07:02:01 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -3,6 +3,10 @@ const newPluginXXXX = axios.create({
   baseURL: "https://a.com",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
+const newPluginXXXX = axios.create({
+  baseURL: "https://a.com",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 const plugin11 = axios.create({
   baseURL:
     "https://crowdbotics-slack-dev.herokuapp.com/dashboard/app/7307/storyboard/8491/",
